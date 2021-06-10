2f8111aa2c7f238f5a21009a3d5e90d5c99ae271
Renan Lima <renan.arantes123@gmail.com>
2021-06-10 14:41:00 -0300

    criado secao sobre nos e servicos

--- assets/src/js/scripts.js
+++ assets/src/js/scripts.js
@@ -1,6 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(".carrossel").slick({
         autoplay: true,
         autoplaySpeed: 3000
     });
+
+
+    $('.galeria').slick({
+        dots: false,
+        infinite: true,
+        arrows: false,
+        speed: 1000,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
 })
